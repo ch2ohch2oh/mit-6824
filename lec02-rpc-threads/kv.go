befb382bd4863013fe33d8256d3cd9cd85443d88
Dazhi Wang <ch2ohch2oh@gmail.com>
2023-10-08 17:03:20 -0400

    Add comment in the kv example

--- lec02-rpc-threads/kv.go
+++ lec02-rpc-threads/kv.go
@@ -76,7 +76,7 @@ func server() {
 	kv := new(KV)
 	kv.data = map[string]string{}
 	rpcs := rpc.NewServer()
-	rpcs.Register(kv)
+	rpcs.Register(kv) // Only methods starting with capital letters are registered
 	l, e := net.Listen("tcp", ":1234")
 	if e != nil {
 		log.Fatal("listen error:", e)
